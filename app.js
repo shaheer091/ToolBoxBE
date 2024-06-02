fb20be3b1ce466879641e25e2cc193e301cad19c
Ahammed Shaheer <shaheerbinmuhammed6@gmail.com>
2024-06-02 22:32:12 +0530

    added cors and rearranged app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 require("dotenv").config();
-const port = process.env.PORT;
-app.use(express.json());
+const port = process.env.PORT
+
+// Import Routes
 const userRoute = require("./routes/user.routes");
 const commonRoute = require("./routes/common.routes");
+
+// Import Database Utility
 const database = require("./utility/database");
 
+// Middleware Setup
+app.use(cors());
+app.use(express.json());
+
+// Route Setup
 app.use("/", commonRoute);
 app.use("/user", userRoute);
 
+// Connect to Database
 database();
 
+// Start Server
 app.listen(port, () => {
-  console.log(`server running on ${port}`);
+  console.log(`Server running on port ${port}`);
 });
